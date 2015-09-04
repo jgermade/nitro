810741164d1f5762845a4fb19330908fb73f1620
Jesús Germade <jesus@germade.es>
2015-09-04 03:11:23 +0200

    added some tweaks

--- nitro.js
+++ nitro.js
@@ -1,13 +1,17 @@
 
-var colors = require('colors'),
+var nitro = {},
+    _ = require('jstools-utils'),
+    colors = require('colors'),
     glob = require('glob'),
+    mmatch = require('minimatch'),
+    Minimatch = mmatch.Minimatch,
     fs = require('fs'),
     path = require('path'),
     JSHINT = require('jshint').JSHINT,
     // childProcess = require('child_process'),
     // spawn = childProcess.spawn,
     shell = require('shelljs'),
-    noop = function () {};
+    noop = function (value) { return value; };
 
 var file = {
       read: function () {
@@ -36,12 +40,6 @@ var file = {
         return fs.existsSync(dirPath);
       }
     };
-
-function cwd () {
-  var paths = [process.cwd()];
-  [].push.apply(paths, arguments);
-  return path.join.apply(null, paths );
-}
 
 function exec(cmd) {
     return shell.exec(cmd);
@@ -88,32 +86,89 @@ function timingSync ( dest, fn ) {
   fn();
   var elapsedTime = timestamp() - start;
   if( dest ) {
-    console.log('\n' + dest, 'updated'.green, ( elapsedTime + 'ms' ).yellow );
+    console.log('\n' + dest, 'updated'.green, ( elapsedTime + 'ms' ).yellow, '\n' );
   }
   return timestamp() - start;
 }
 
-function GlobFile (src, data) {
-  if( !data ) {
-    var matches = src.match(/(.*)\/([^\/]+)/);
 
-    this.fileName = ( matches && matches[2] ) || src;
-    this.filePath = ( matches && matches[1] ) || null;
-    this.src = file.read(src);
-  } else if( data && data.fileName ) {
-    this.fileName = data.fileName;
-    this.filePath = data.filePath;
-    this.src = src;
+var processors = {},
+    presets = {},
+    requireLibs = function (requirements) {
+      requirements.forEach(function (libName) {
+        if( !dir.exists('node_modules/' + libName) ) {
+          exec('npm install ' + libName);
+        }
+      });
+    },
+    addPreset = function (processorKey, processor, isCollection, requirements) {
+      presets[processorKey] = function () {
+        if( requirements && requirements.length ) {
+          requireLibs(requirements);
+
+          if( !processors[processorKey] ) {
+            nitro.fileProcessor(processorKey, processor, isCollection);
+          }
+        }
+      };
+    },
+    loadPresets = function () {
+      [].forEach.call(arguments, function (preset) {
+        if( !presets[preset] ) {
+          throw new Error('preset not found: ' + preset);
+        }
+
+        presets[preset]();
+      });
+    };
+
+addPreset('log', function (src, fileName, filePath) {
+  console.log('fileLog', src, fileName, filePath);
+  return src;
+});
+
+addPreset('sass', function (src) {
+  return require('node-sass').renderSync({ data: src }).css;
+}, false, ['node-sass']);
+
+addPreset('uglify', function (src) {
+  return require('uglify-js').minify(src, { fromString: true }).code;
+}, false, ['uglify-js']);
+
+function parsePath (filePath) {
+  var matches = filePath.match(/(.*)\/([^\/]+)/);
+
+  return {
+    fileName: ( matches && matches[2] ) || filePath,
+    filePath: ( matches && matches[1] ) || null
+  };
+}
+
+function GlobFile (src, data) {
+  if( data && data.fileName ) {
+    _.extend(this, data, { src: src });
+  } else {
+    var cwd = data || '.';
+    _.extend(this, parsePath(src), { src: file.read( path.join(cwd, src) ) });
   }
 }
+
+GlobFile.prototype.getFullPath = function () {
+  return path.join( this.filePath || '.', this.fileName );
+};
 
 function fileByName (fileName) {
   return new GlobFile(fileName);
 }
 
-function GlobFiles (src) {
+function GlobFiles (src, options) {
+  options = options || {};
+  var cwd = options.cwd || '.';
+
   if( typeof src === 'string' ) {
-    [].push.apply(this, glob.sync(src).map(fileByName) );
+    [].push.apply( this, glob.sync(src, options || {}).map(function (filePath) {
+      return new GlobFile( file.read( path.join(cwd, filePath) ), parsePath(filePath) );
+    }) );
   } else if ( src instanceof Array ) {
     [].push.apply(this, src);
   }
@@ -126,22 +181,70 @@ function getFileSrc (f) {
 GlobFiles.prototype = [];
 GlobFiles.prototype.each = GlobFiles.prototype.forEach;
 
-GlobFiles.prototype.append = function (files) {
+GlobFiles.prototype.add = function (files) {
   [].push.apply(this, files);
   return this;
 };
 
-GlobFiles.prototype.concat = function (fileName) {
-  if( fileName ) {
-    return new GlobFiles([new GlobFile(this.concat(), { fileName: fileName })]);
+GlobFiles.prototype.process = function (processorKey) {
+  if( !processors[processorKey] ) {
+    throw new Error('file processor missing: ' + processorKey);
+  }
+
+  // console.log('processor', processorKey, processors[processorKey], this);
+
+  return processors[processorKey].call(this);
+};
+
+GlobFiles.prototype.concat = function (filter, filePath) {
+  if( !filePath ) {
+    if( !filter ) {
+      throw new Error('GlobFiles.concat :: file path needs to be defined');
+    }
+    filePath = filter;
+  }
+
+
+  if( filePath !== true ) {
+    var files = new GlobFiles();
+
+    files.push( new GlobFile(this.concat(true), parsePath(filePath) ) );
+    return files;
   } else {
     return this.map(getFileSrc).join('');
   }
 };
 
-GlobFiles.prototype.write = function (dest) {
-  this.forEach(function (file) {
-    file.write( path.join(dest, file.filePath, file.fileName), this.src );
+function filterPattern (pattern) {
+  var mm = new Minimatch(pattern);
+  return function (f) {
+    return mm.match( f.getFullPath() );
+  };
+}
+
+GlobFiles.prototype.filter = function (pattern) {
+  return new GlobFiles( [].filter(this,  filterPattern(pattern) ) );
+};
+
+GlobFiles.prototype.with = function (pattern, processor) {
+  var matchPattern = filterPattern(pattern),
+      pristine = new GlobFiles(),
+      dirty = new GlobFiles();
+
+  for( var i = 0, n = this.length; i < n ; i++ ) {
+    if( matchPattern(this[i]) ) {
+      dirty.push(this[i]);
+    } else {
+      pristine.push(this[i]);
+    }
+  }
+
+  return pristine.concat( processor(dirty) );
+};
+
+GlobFiles.prototype.write = function (dest, rename) {
+  this.forEach(function (f) {
+    file.write( ( rename || noop )( path.join(dest, f.filePath || '.', f.fileName) ), f.src );
   });
 };
 
@@ -150,15 +253,38 @@ GlobFiles.prototype.writeFile = function (destFile) {
   return this;
 };
 
-function copyFiles (globSrc, dest, options) {
-  glob.sync(globSrc, options).forEach(function (filePath) {
-    file.copy( path.join(options.cwd || '.', filePath) , path.join(dest || '.', filePath) );
+function copyFiles (cwd, globSrc, dest, options) {
+  glob.sync(globSrc, _.extend( options || {}, cwd ? { cwd: cwd } : undefined ) ).forEach(function (filePath) {
+    file.copy( path.join(cwd || options.cwd || '.', filePath) , path.join(dest || '.', filePath) );
   });
   return nitro;
 }
 
-var nitro = {
-  cwd: cwd,
+function CwdPath () {
+  var pcwd = path.join.apply(null, [].slice.call(arguments) );
+
+  this.path = pcwd;
+  this.cwd = path.join( process.cwd(), pcwd );
+}
+
+CwdPath.prototype.get = function () {
+  return this.cwd;
+};
+
+CwdPath.prototype.load = function (globSrc) {
+  return new GlobFiles(globSrc, { cwd: this.path });
+};
+
+function getCwd (p) {
+  return new CwdPath(p);
+}
+
+getCwd.path = function () {
+  return path.join.apply(null, [process.cwd()].concat( [].slice.call(arguments) ) );
+};
+
+module.exports = _.extend(nitro, {
+  cwd: getCwd,
   exec: exec,
   glob: glob,
   dir: dir,
@@ -167,16 +293,24 @@ var nitro = {
   timestamp: timestamp,
   timingSync: timingSync,
   jshint: launchJShint,
-  load: function ( globSrc ) {
-    return new GlobFiles(globSrc);
+  load: function ( globSrc, options ) {
+    return new GlobFiles(globSrc, options);
   },
-  fileProcessor: function (methodName, processor, isCollection) {
+  autoLoad: loadPresets,
+  requireLibs: function () {
+    requireLibs( [].slice.call(arguments) );
+  },
+  fileProcessor: function (methodName, processor, isCollection, requirements) {
+    if( requirements ) {
+      requireLibs(requirements);
+    }
+
     if( isCollection ) {
-      GlobFiles.prototype[methodName] = function () {
+      processors[methodName] = function () {
         return new GlobFiles( processor(this) || [] );
       };
     } else {
-      GlobFiles.prototype[methodName] = function () {
+      processors[methodName] = function () {
         var files = new GlobFiles(), f;
 
         for( var i = 0, n = this.length; i < n ; i++ ) {
@@ -188,8 +322,7 @@ var nitro = {
         return files;
       };
     }
+    // GlobFiles.prototype[methodName] = processors[methodName];
     return nitro;
   }
-};
-
-module.exports = nitro;
+});
