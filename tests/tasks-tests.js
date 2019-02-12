24b42694eac6e1573c4ed377e902cb7ef2689807
Jesús Germade <jesus@germade.es>
2019-02-12 13:32:32 +0100

    Added some tests

--- tests/tasks-tests.js
+++ tests/tasks-tests.js
@@ -16,6 +16,27 @@ describe('Tasks', function() {
     assert(!pristine);
   });
 
+  it('task list definition', function (done) {
+    var pristine2 = true,
+        pristine3 = true
+
+    nitro.task('test2', function () {
+      pristine2 = false;
+    });
+
+    nitro.task('test3', function () {
+      pristine3 = false;
+    });
+
+    nitro.task(['test2', 'test3']).then(function () {
+      assert.strictEqual(pristine2, false)
+      assert.strictEqual(pristine3, false)
+    })
+    .then(done, done)
+
+    
+  });
+
   // @TODO test async tasks
   // it('task nested', function (done) {
   //   var counter = 0, steps = [];
